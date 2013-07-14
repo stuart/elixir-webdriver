20c2a4b52bcfebb174a1fc19d5c8e2e562172add
Stuart Coyle <stuart.coyle@gmail.com>
2013-07-14 20:01:23 +1000

    License and EXPM package.

---
+++ package.exs
@@ -0,0 +1,12 @@
+Expm.Package.new( name: "webdriver", 
+                  description: "Elixir WebDriver library",
+                  version: "0.0.3", 
+                  keywords: ["Elixir", "Selenium", "WebDriver", "library", "testing"], 
+                  homepage: "http://stuart.github.io/elixir-webdriver", 
+                  maintainers: [[   name: "Stuart Coyle", 
+                                   email: "stuart.coyle@gmail.com"]],
+                  repositories: [[ github: "stuart/elixir-webdriver"]],
+                  dependencies: ["httpotion", "jsonex", "mock"],
+                  licenses: ["MIT License, Apache License"]
+                 )
+    
