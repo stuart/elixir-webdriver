43d0385ff55ccf29cbec95e202823dd7421f2168
Stuart Coyle <stuart.coyle@gmail.com>
2014-03-02 16:07:56 +1100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WebDriver.Mixfile do
 
   def project do
     [ app: :webdriver,
-      version: "0.4.0",
+      version: "0.4.1",
       source_url: "https://github.com/stuart/elixir-webdriver",
       homepage_url: "http://stuart.github.io/elixir-webdriver",
       env: [
--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new( name: "webdriver",
                   description: "Elixir WebDriver library",
-                  version: "0.4.0",
+                  version: "0.4.1",
                   keywords: ["Elixir", "Selenium", "WebDriver", "library", "testing"],
                   homepage: "http://stuart.github.io/elixir-webdriver",
                   maintainers: [[  name: "Stuart Coyle",
