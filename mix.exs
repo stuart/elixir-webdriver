ce3c41d0e426934de1a5d5c2d94ea3ef80527912
Stuart Coyle <stuart.coyle@gmail.com>
2014-08-16 12:38:17 +1000

    Added description for hex.pm

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule WebDriver.Mixfile do
   def project do
     [ app: :webdriver,
       version: "0.5.0",
+      description: "Webdriver protocol for driving web browsers."
       source_url: "https://github.com/stuart/elixir-webdriver",
       homepage_url: "http://stuart.github.io/elixir-webdriver",
       package: package,
