4ec58316eae9da702e4cab96fa8a6b8141baad55
Stuart Coyle <stuart.coyle@gmail.com>
2014-08-17 17:48:54 +1000

    Task to fetch webdriver.xpi

--- lib/mix/tasks/webdriver.ex
+++ lib/mix/tasks/webdriver.ex
@@ -1,9 +1,20 @@
 defmodule Mix.Tasks.Webdriver do
-  defmodule Firefox do
+  defmodule Firefox.Install do
     use Mix.Task
+
+    @download_url "https://raw.githubusercontent.com/stuart/elixir-webdriver/feature/load_firefox_plugin_task/plugins/firefox/webdriver.xpi"
+
     def run(_) do
       Mix.shell.info "Downloading the Firefox Webdriver plugin."
-      
+      File.mkdir_p Path.dirname(plugin_path)
+      {data, _} = System.cmd "curl",[@download_url]
+      :ok = File.write plugin_path, data
+      Mix.shell.info "Done."
+      :ok
+    end
+
+    defp plugin_path
+      Path.join [__DIR__, "..", "..", "..", "plugins", "firefox","webdriver.xpi"]
     end
   end
 end
